aa52560cee38aa211c0e0c361f8bfc5945124d97
Seweryn Golba <sgoolb@gmail.com>
2023-10-09 20:47:44 +0200

    selection display

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const submitBtn = document.querySelector("#submit-button");
 const ratingPage = document.querySelector(".first-page");
 const thanksPage = document.querySelector(".thank-you");
+const ratingElement = document.querySelectorAll(".rating");
+const yourSelection = document.querySelector(".your-selection");
+
+ratingElement.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    ratingElement.forEach((r) => r.classList.remove("active"));
+    rate.classList.add("active");
+
+    const selectedRating = rate.textContent;
+
+    yourSelection.textContent = `You selected ${selectedRating} out of 5`;
+  });
+});
 
 submitBtn.addEventListener("click", () => {
   ratingPage.style.display = "none";
